810eb956d6e76202bf9aa52f90c5b691fc008e0c
CWeafer-D <83568313+CWeafer-D@users.noreply.github.com>
2021-06-02 01:20:47 -0700

    Added azure and docker implementation

--- BakeryWebUI/Models/CustomerVM.cs
+++ BakeryWebUI/Models/CustomerVM.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-
+using System.ComponentModel.DataAnnotations;
+using SModel;
 namespace BakeryWebUI.Models
 {
     /// <summary>
@@ -10,6 +11,26 @@ namespace BakeryWebUI.Models
     /// </summary>
     public class CustomerVM
     {
+        public CustomerVM(Customer customer)
+        {
+            Id = customer.Id;
+            FirstName = customer.FirstName;
+            LastName = customer.LastName;
+
+        }
+        public CustomerVM()
+        {
+
+        }
+        public int Id { get; set; }
+
+
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+
+        public List<Orders> OrderList { get; set; }
 
     }
 }
